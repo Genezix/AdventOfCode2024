9fcaacb093b739a3bbf998c2f5590ea16aafcf69
Florian <mail.de.florian.kaiser@gmail.com>
2024-12-02 12:08:54 +0100

    day2

--- src/main/kotlin/adventofcode2024/day2/ProgramDay2.kt
+++ src/main/kotlin/adventofcode2024/day2/ProgramDay2.kt
@@ -1,11 +1,21 @@
 package adventofcode2024.day2
 
 import common.Program
+import kotlin.math.abs
 
 class ProgramDay2(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val inputs = brutInputs.map { it.split(' ').map { it.toLong() } }
 
-    override fun part1(): String = ""
+    override fun part1(): String = inputs.count { it.isValid() }.toString()
 
-    override fun part2(): String = ""
+    override fun part2(): String = inputs.count { it.isValid() || it.isValidWithChance() }.toString()
+
+    private fun List<Long>.isValidWithChance() = List(this.size) { removedIndex ->
+        this.filterIndexed { index, _ -> index != removedIndex }
+    }.any { it.isValid() }
+
+    private fun List<Long>.isValid() =
+        this.zipWithNext { a, b ->
+            abs(a - b)
+        }.all { it in 1..3 } && (this == this.sorted() || this == this.sortedDescending())
 }
--- src/test/kotlin/adventofcode2024/day2/Day2KtTest.kt
+++ src/test/kotlin/adventofcode2024/day2/Day2KtTest.kt
@@ -6,7 +6,12 @@ import org.junit.Test
 internal class Day2KtTest {
     private val program = ProgramDay2(
         brutInputs = listOf(
-            ""
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9",
         ).map { it },
         debug = true
     )
@@ -14,7 +19,7 @@ internal class Day2KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "2",
             actual = program.part1()
         )
     }
@@ -22,7 +27,7 @@ internal class Day2KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "4",
             actual = program.part2()
         )
     }
