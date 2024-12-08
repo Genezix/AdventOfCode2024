ed08d926e16ced8e28b8a17a059a6b6a95cd353e
Florian Kaiser <florian_kaiser@ext.connect-tech.sncf>
2024-12-08 15:25:47 +0100

    day7

--- src/main/kotlin/adventofcode2024/day7/ProgramDay7.kt
+++ src/main/kotlin/adventofcode2024/day7/ProgramDay7.kt
@@ -3,9 +3,54 @@ package adventofcode2024.day7
 import common.Program
 
 class ProgramDay7(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val calculs = brutInputs.map { Calcul.build(it) }
 
-    override fun part1(): String = ""
+    override fun part1(): String {
+        val operators = listOf("+", "*")
+        return calculs.mapNotNull { isValid(it, operators) }.sum().toString()
+    }
 
-    override fun part2(): String = ""
+    override fun part2(): String {
+        val operators = listOf("+", "*", "||")
+        return calculs.mapNotNull { isValid(it, operators) }.sum().toString()
+    }
+
+    private fun isValid(calcul: Calcul, operators: List<String>): Long? {
+        val isValid = calcul.numbers.flatMapIndexed { index, number ->
+            operation(number, calcul.numbers.drop(index + 1), operators)
+        }.any { it == calcul.result }
+
+        return calcul.result.takeIf { isValid }
+    }
+
+    private fun operation(init: Long, numbers : List<Long>, operators: List<String>) : List<Long> {
+        if(numbers.isEmpty()) return listOf(init)
+
+        val number = numbers.first()
+
+        return operators.flatMap { operator ->
+            operation(
+                init = when(operator) {
+                    "+" -> init + number
+                    "*" -> init * number
+                    "||" -> "$init$number".toLong()
+                    else -> throw IllegalArgumentException("Unknown operator $operator")
+                },
+                numbers = numbers.drop(1),
+                operators = operators
+            )
+        }
+    }
+
+    data class Calcul(val result: Long, val numbers : List<Long>) {
+        companion object {
+            fun build(string: String): Calcul {
+                val split = string.split(": ")
+                return Calcul(
+                    result = split.first().toLong(),
+                    numbers = split.last().split(" ").map { it.toLong() }
+                )
+            }
+        }
+    }
 }
--- src/test/kotlin/adventofcode2024/day7/Day7KtTest.kt
+++ src/test/kotlin/adventofcode2024/day7/Day7KtTest.kt
@@ -6,7 +6,15 @@ import org.junit.Test
 internal class Day7KtTest {
     private val program = ProgramDay7(
         brutInputs = listOf(
-            ""
+            "190: 10 19",
+            "3267: 81 40 27",
+            "83: 17 5",
+            "156: 15 6",
+            "7290: 6 8 6 15",
+            "161011: 16 10 13",
+            "192: 17 8 14",
+            "21037: 9 7 18 13",
+            "292: 11 6 16 20",
         ).map { it },
         debug = true
     )
@@ -14,7 +22,7 @@ internal class Day7KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "3749",
             actual = program.part1()
         )
     }
@@ -22,7 +30,7 @@ internal class Day7KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "11387",
             actual = program.part2()
         )
     }
